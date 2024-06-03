185b466e9d33b49e894c13bf5c370a3cc3b9f80b
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-06-03 14:39:11 +0900

    init:setup project

---
+++ src/main/java/com/joy/photobooth_be/PhotoboothBeApplication.java
@@ -0,0 +1,13 @@
+package com.joy.photobooth_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoboothBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoboothBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joy/photobooth_be/PhotoboothBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joy.photobooth_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoboothBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
